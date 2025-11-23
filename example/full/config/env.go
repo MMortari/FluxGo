68fe889bad801daeddc9ffb6b7c6ad0f8b15767b
MMortari <bmortari2000@gmail.com>
2025-11-23 18:48:53 -0300

    doc: create Env struct for environment configuration and update Module function to utilize it

---
+++ example/full/config/env.go
@@ -0,0 +1,22 @@
+package config
+
+import fluxgo "github.com/MMortari/FluxGo"
+
+type Env struct {
+	fluxgo.Env
+	Database struct {
+		Dsn string `env:"DATABASE_DSN" validate:"required"`
+	}
+	Redis struct {
+		Addr string `env:"REDIS_ADDR" validate:"required"`
+	}
+	Logger struct {
+		Type     string `env:"LOGGER_TYPE" validate:"required"`
+		Level    string `env:"LOGGER_LEVEL" validate:"required"`
+		FilePath string `env:"LOGGER_FILE_PATH" validate:"required"`
+	}
+	Apm struct {
+		Exporter     string `env:"APM_EXPORTER" validate:"required"`
+		CollectorUrl string `env:"APM_COLLECTOR_URL" validate:"required"`
+	}
+}
--- example/full/shared/module/main.go
+++ example/full/shared/module/main.go
@@ -2,38 +2,21 @@ package module
 
 import (
 	fluxgo "github.com/MMortari/FluxGo"
+	"github.com/MMortari/FluxGo/example/full/config"
 	"github.com/MMortari/FluxGo/example/full/modules/user"
 	"github.com/MMortari/FluxGo/example/full/shared/http"
 	"github.com/MMortari/FluxGo/example/full/shared/repositories"
 	"github.com/redis/go-redis/v9"
 )
 
-type Env struct {
-	Database struct {
-		Dsn string `env:"DATABASE_DSN" validate:"required"`
-	}
-	Redis struct {
-		Addr string `env:"REDIS_ADDR" validate:"required"`
-	}
-	Logger struct {
-		Type     string `env:"LOGGER_TYPE" validate:"required"`
-		Level    string `env:"LOGGER_LEVEL" validate:"required"`
-		FilePath string `env:"LOGGER_FILE_PATH" validate:"required"`
-	}
-	Apm struct {
-		Exporter     string `env:"APM_EXPORTER" validate:"required"`
-		CollectorUrl string `env:"APM_COLLECTOR_URL" validate:"required"`
-	}
-}
-
 func Module() *fluxgo.FluxGo {
-	env := fluxgo.ParseEnv[Env](fluxgo.EnvOptions{LoadFromFile: fluxgo.Pointer(".env.development"), Validate: true})
+	env := fluxgo.ParseEnv[config.Env](fluxgo.EnvOptions{LoadFromFile: fluxgo.Pointer(".env.development"), Validate: true})
 
-	flux := fluxgo.New(fluxgo.FluxGo{Name: "Teste Full", Version: "1", Env: "development", Debugger: true})
+	flux := fluxgo.New(fluxgo.FluxGo{Name: "Teste Full", Version: "1", Env: &env.Env, Debugger: true})
 	flux.AddApm(fluxgo.ApmOptions{CollectorURL: env.Apm.CollectorUrl, Exporter: env.Apm.Exporter})
 	flux.ConfigLogger(fluxgo.LoggerOptions{Type: env.Logger.Type, Level: env.Logger.Level, LogFilePath: env.Logger.FilePath})
 
-	flux.AddDependency(func() *Env { return &env })
+	flux.AddDependency(func() *config.Env { return &env })
 	flux.AddDatabase(fluxgo.DatabaseOptions{Dsn: env.Database.Dsn})
 	flux.AddRedis(fluxgo.RedisOptions{Options: redis.Options{Addr: env.Redis.Addr}})
 	flux.AddCron()
