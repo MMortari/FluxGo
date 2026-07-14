5c51414f17a379c4a02e644db471934dc15b8cde
MMortari <bmortari2000@gmail.com>
2026-07-14 09:27:48 -0300

    fix: missing metrics functions

--- metrics.go
+++ metrics.go
@@ -12,25 +12,31 @@ import (
 	"go.uber.org/fx"
 )
 
-type FluxGoMetrics struct {
-	Provider     *sdkmetric.MeterProvider
-	meter        metric.Meter
-	counterMap   map[string]metric.Int64Counter
-	histogramMap map[string]metric.Float64Histogram
-	gaugeMap     map[string]metric.Float64Gauge
-	mutex        *sync.RWMutex
+type Metrics struct {
+	Provider          *sdkmetric.MeterProvider
+	meter             metric.Meter
+	counterIntMap     map[string]metric.Int64Counter
+	counterFloatMap   map[string]metric.Float64Counter
+	histogramIntMap   map[string]metric.Int64Histogram
+	histogramFloatMap map[string]metric.Float64Histogram
+	gaugeIntMap       map[string]metric.Int64Gauge
+	gaugeFloatMap     map[string]metric.Float64Gauge
+	mutex             *sync.RWMutex
 }
 
 func (f *FluxGo) AddMetrics() *FluxGo {
-	f.AddDependency(func() *FluxGoMetrics {
-		return &FluxGoMetrics{
-			counterMap:   make(map[string]metric.Int64Counter),
-			histogramMap: make(map[string]metric.Float64Histogram),
-			gaugeMap:     make(map[string]metric.Float64Gauge),
-			mutex:        &sync.RWMutex{},
+	f.AddDependency(func() *Metrics {
+		return &Metrics{
+			counterIntMap:     make(map[string]metric.Int64Counter),
+			counterFloatMap:   make(map[string]metric.Float64Counter),
+			histogramIntMap:   make(map[string]metric.Int64Histogram),
+			histogramFloatMap: make(map[string]metric.Float64Histogram),
+			gaugeIntMap:       make(map[string]metric.Int64Gauge),
+			gaugeFloatMap:     make(map[string]metric.Float64Gauge),
+			mutex:             &sync.RWMutex{},
 		}
 	})
-	f.AddInvoke(func(lc fx.Lifecycle, m *FluxGoMetrics, o *Otel) error {
+	f.AddInvoke(func(lc fx.Lifecycle, m *Metrics, o *Otel) error {
 		lc.Append(fx.Hook{
 			OnStart: func(ctx context.Context) error {
 				var exporter sdkmetric.Exporter
@@ -71,20 +77,48 @@ func (f *FluxGo) AddMetrics() *FluxGo {
 	return f
 }
 
-func (m *FluxGoMetrics) NewInt64Counter(name, description string) metric.Int64Counter {
+func (m *Metrics) NewIntCounter(name, description string) metric.Int64Counter {
 	counter, err := m.meter.Int64Counter(name, metric.WithDescription(description))
 	if err != nil {
 		panic(err)
 	}
 
 	m.mutex.Lock()
-	m.counterMap[name] = counter
+	m.counterIntMap[name] = counter
+	m.mutex.Unlock()
+
+	return counter
+}
+func (m *Metrics) NewFloatCounter(name, description string) metric.Float64Counter {
+	counter, err := m.meter.Float64Counter(name, metric.WithDescription(description))
+	if err != nil {
+		panic(err)
+	}
+
+	m.mutex.Lock()
+	m.counterFloatMap[name] = counter
 	m.mutex.Unlock()
 
 	return counter
 }
 
-func (m *FluxGoMetrics) NewFloat64Histogram(name, description string, boundaries []float64) metric.Float64Histogram {
+func (m *Metrics) NewIntHistogram(name, description string, boundaries []float64) metric.Int64Histogram {
+	hist, err := m.meter.Int64Histogram(
+		name,
+		metric.WithDescription(description),
+		metric.WithExplicitBucketBoundaries(boundaries...),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	m.mutex.Lock()
+	m.histogramIntMap[name] = hist
+	m.mutex.Unlock()
+
+	return hist
+}
+func (m *Metrics) NewFloatHistogram(name, description string, boundaries []float64) metric.Float64Histogram {
 	hist, err := m.meter.Float64Histogram(
 		name,
 		metric.WithDescription(description),
@@ -95,39 +129,66 @@ func (m *FluxGoMetrics) NewFloat64Histogram(name, description string, boundaries
 	}
 
 	m.mutex.Lock()
-	m.histogramMap[name] = hist
+	m.histogramFloatMap[name] = hist
 	m.mutex.Unlock()
 
 	return hist
 }
 
-func (m *FluxGoMetrics) NewFloat64Gauge(name, description string) metric.Float64Gauge {
+func (m *Metrics) NewIntGauge(name, description string) metric.Int64Gauge {
+	gauge, err := m.meter.Int64Gauge(name, metric.WithDescription(description))
+	if err != nil {
+		panic(err)
+	}
+
+	m.mutex.Lock()
+	m.gaugeIntMap[name] = gauge
+	m.mutex.Unlock()
+
+	return gauge
+}
+func (m *Metrics) NewFloatGauge(name, description string) metric.Float64Gauge {
 	gauge, err := m.meter.Float64Gauge(name, metric.WithDescription(description))
 	if err != nil {
 		panic(err)
 	}
 
 	m.mutex.Lock()
-	m.gaugeMap[name] = gauge
+	m.gaugeFloatMap[name] = gauge
 	m.mutex.Unlock()
 
 	return gauge
 }
 
-func (m *FluxGoMetrics) GetCounter(name string) metric.Int64Counter {
+func (m *Metrics) GetCounterInt(name string) metric.Int64Counter {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
-	return m.counterMap[name]
+	return m.counterIntMap[name]
+}
+func (m *Metrics) GetCounterFloat(name string) metric.Float64Counter {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	return m.counterFloatMap[name]
 }
 
-func (m *FluxGoMetrics) GetHistogram(name string) metric.Float64Histogram {
+func (m *Metrics) GetHistogramInt(name string) metric.Int64Histogram {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
-	return m.histogramMap[name]
+	return m.histogramIntMap[name]
+}
+func (m *Metrics) GetHistogramFloat(name string) metric.Float64Histogram {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	return m.histogramFloatMap[name]
 }
 
-func (m *FluxGoMetrics) GetGauge(name string) metric.Float64Gauge {
+func (m *Metrics) GetGaugeInt(name string) metric.Int64Gauge {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
-	return m.gaugeMap[name]
+	return m.gaugeIntMap[name]
+}
+func (m *Metrics) GetGaugeFloat(name string) metric.Float64Gauge {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	return m.gaugeFloatMap[name]
 }
